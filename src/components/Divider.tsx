a288c4ba6421b1f8d67e9f921c24c9e5fcbd319f
han kim <hankim@hans-Mac-mini.local>
2026-01-31 18:14:42 -0500

    Add Divider component

---
+++ src/components/Divider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface DividerProps {
+  className?: string;
+}
+
+export const Divider: React.FC<DividerProps> = ({ className = '' }) => {
+  return <hr className={`border-gray-300 ${className}`} />;
+};
+
+export default Divider;
